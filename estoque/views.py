b3bf6c1ba6cd1565e04fc4d783d1a6c653c0b12f
ikedayuji <ikedayuji.2002@gmail.com>
2025-08-14 14:20:32 -0300

    Listagem de Produtos / Cadastrando Produtos

--- estoque/views.py
+++ estoque/views.py
@@ -1,13 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Produto
 
 def listar_produtos(request):
     return render(request, 'listar_produtos.html')
 
 def cadastrar_produto(request):
+    if request.method == "POST":
+        nome = request.POST.get('nome')
+        quantidade = request.POST.get('quantidade')
+        preco = request.POST.get('preco').replace('R$', '').replace(',', '.').strip()
+
+        Produto.objects.create(
+            nome=nome,
+            quantidade=quantidade,
+            preco=preco
+        )
+        return redirect('listar_produtos')
+
     return render(request, 'cadastrar_produto.html')
+
+def listar_produtos(request):
+    produtos = Produto.objects.all()
+    return render(request, 'listar_produtos.html', {'produtos': produtos})
+
 
 def movimentacoes(request):
     return render(request, 'movimentacoes.html')
 
 def relatorios(request):
     return render(request, 'relatorios.html')
+
+from django.shortcuts import render, redirect
+from .models import Produto
+
+
+def listar_produtos(request):
+    produtos = Produto.objects.all()
+    return render(request, 'listar_produtos.html', {'produtos': produtos})
--- estoque_project/urls.py
+++ estoque_project/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', views.listar_produtos, name='listar_produtos'),
+    path('admin/', admin.site.urls),
     path('cadastrar/', views.cadastrar_produto, name='cadastrar_produto'),
+    path('listar/', views.listar_produtos, name='listar_produtos'),
     path('movimentacoes/', views.movimentacoes, name='movimentacoes'),
     path('relatorios/', views.relatorios, name='relatorios'),
 ]

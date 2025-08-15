6f9edf9f05ad99fc08f8bd8c9e3dc18b7025ab04
ikedayuji <ikedayuji.2002@gmail.com>
2025-08-14 23:42:42 -0300

    Produtos sendo mostrado no admin

--- estoque/views.py
+++ estoque/views.py
@@ -3,7 +3,8 @@ from .models import Produto
 from django.db.models import Sum
 
 def listar_produtos(request):
-    return render(request, 'listar_produtos.html')
+    produtos = Produto.objects.all()
+    return render(request, 'listar_produtos.html', {'produtos': produtos})
 
 def cadastrar_produto(request):
     if request.method == "POST":
@@ -13,23 +14,33 @@ def cadastrar_produto(request):
 
         Produto.objects.create(
             nome=nome,
-            quantidade=quantidade,
-            preco=preco
+            quantidade=int(quantidade),
+            preco=float(preco)
         )
         return redirect('listar_produtos')
 
     return render(request, 'cadastrar_produto.html')
 
-def listar_produtos(request):
-    produtos = Produto.objects.all()
-    return render(request, 'listar_produtos.html', {'produtos': produtos})
-
-
 def movimentacoes(request):
     return render(request, 'movimentacoes.html')
 
 def relatorios(request):
-    return render(request, 'relatorios.html')
+    produtos = Produto.objects.all()
+
+    total_itens = produtos.aggregate(Sum('quantidade'))['quantidade__sum'] or 0
+    valor_total = sum(p.quantidade * p.preco for p in produtos)
+
+    nomes = [p.nome for p in produtos]
+    quantidades = [p.quantidade for p in produtos]
+
+    context = {
+        'produtos': produtos,
+        'total_itens': total_itens,
+        'valor_total': valor_total,
+        'nomes': nomes,
+        'quantidades': quantidades
+    }
+    return render(request, 'relatorios.html', context)
 
 def listar_produtos(request):
     produtos = Produto.objects.all()

c458f2810050a097ae645772b76071d15b97fcfc
ikedayuji <ikedayuji.2002@gmail.com>
2025-08-14 23:46:47 -0300

    Correção URLS

--- estoque_project/urls.py
+++ estoque_project/urls.py
@@ -3,10 +3,8 @@ from django.urls import path
 from estoque import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('', views.listar_produtos, name='listar_produtos'),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  
+    path('', views.listar_produtos, name='listar_produtos'), 
     path('cadastrar/', views.cadastrar_produto, name='cadastrar_produto'),
     path('listar/', views.listar_produtos, name='listar_produtos'),
     path('movimentacoes/', views.movimentacoes, name='movimentacoes'),
